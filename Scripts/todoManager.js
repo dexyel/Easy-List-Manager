1a177a03901c1245a46f53bdfe1d3cba8a0f3492
dexyel <39320381+dexyel@users.noreply.github.com>
2023-01-23 23:46:27 +0100

    add buttons tooltips (native)

--- Scripts/todoManager.js
+++ Scripts/todoManager.js
@@ -120,7 +120,7 @@ for (let i = 0; i < circles.length; i++) {
     circles[i].style.color = colors[i];
 } //attribue les couleurs des labels aux boutons de couleurs de l'input
 
-initials.forEach(initial => {
+initials.forEach((initial) => {
     let randomIndex = Math.floor(Math.random() * colors.length);
     initial.style.color = colors[randomIndex]; 
 }); //attribue une couleur aléatoire aux initiales des titres
@@ -190,115 +190,6 @@ function toggleInterface() {
         }, 200);
     }
 } //affiche l'interface
-
-function toggleFilter() {
-    if (!filterMenu.classList.contains('open')) {
-        filterMenu.classList.replace('closed', 'open');
-        filterButton.classList.add('is-active');
-
-        if (!filtersEventsInitiated) {
-            loadFiltersNames();
-            filtersEventsInitiated = true;
-        }        
-    }
-    else {
-        resetFilters();
-        showAllItems();
-        
-        filterMenu.classList.replace('open', 'closed');
-        filterButton.classList.remove('is-active');
-    }
-} //ouvre le menu des filtres
-
-function loadFiltersNames() {
-    const filterOptions = filterMenu.querySelectorAll('span');
-
-    filterOptions.forEach((filter, index) => {
-        const hoverHandler = () => {
-            filter.style.backgroundColor = colors[index];
-        }
-
-        const unhoverHandler = () => {
-            if (!filter.classList.contains('is-active')) {
-                filter.style.backgroundColor = 'transparent';
-            }
-        }
-
-        const clickHandler = () => {            
-            if (filter === activeFilter && filter.classList.contains('is-active')) {
-                filter.classList.remove('is-active');
-                filter.style.backgroundColor = 'transparent';
-
-                filterOptions.forEach(f => f.style.opacity = 1);
-
-                activeFilter = null;
-
-                showAllItems();
-            }
-            else {
-                filterOptions.forEach(f => {
-                    f.classList.remove('is-active');
-                    f.style.backgroundColor = 'transparent';
-                    f.style.opacity = 0.7;
-                });
-
-                filter.classList.add('is-active');
-                filter.style.backgroundColor = colors[index];
-                filter.style.opacity = 1;
-                activeFilter = filter;
-
-                activateFilter(activeFilter);
-            }
-        }
-
-        if ($.data(filter, 'events')) {
-            $(filter).off('mouseover', hoverHandler);        
-            $(filter).off('mouseout', unhoverHandler);        
-            $(filter).off('click', clickHandler);
-        } //supprimer les events
-
-        filter.addEventListener('mouseover', hoverHandler);    
-        filter.addEventListener('mouseout', unhoverHandler);    
-        filter.addEventListener('click', clickHandler);
-    });
-} //récupère les noms des labels et ajoute les listeners aux filtres 
-
-function activateFilter(activeFilter) {
-    const listItems = document.querySelectorAll('#list-container li');
-
-    listItems.forEach((item) => {
-        const labelText = item.querySelector('#label-modal p').innerText;
-        
-        if (labelText === activeFilter.innerText) {
-            item.style.display = 'flex';
-        }
-        else {
-            item.style.display = 'none';
-        }
-    })
-} //active le filtre choisi
-
-function showAllItems() {
-    const listItems = document.querySelectorAll('#list-container li');
-
-    listItems.forEach((item) => {
-        item.style.display = 'flex';
-    });
-} //montre tous les items
-
-function resetFilters() {
-    const filtersLabel = filterMenu.querySelectorAll('span');
-
-    filtersLabel.forEach(filter => {
-        filter.classList.remove('is-active');
-        filter.style.backgroundColor = 'transparent';
-        filter.style.opacity = 1;
-    });
-
-    activeFilter = null;
-
-       
-} //reset les filtres
 
 function addItemOnClick() {
     if (input.value.length > 0) {
@@ -564,7 +455,7 @@ function checkExisting(data, listData, labelData) {
             return listData.some(newItem => newItem.text === item.text);
         });
 
-        listData.forEach(newItem => {
+        listData.forEach((newItem) => {
             if (!data.items.some(item => item.text === newItem.text)) {
                 data.items.push(newItem);
                 hasChanged = true;
@@ -580,7 +471,7 @@ function checkExisting(data, listData, labelData) {
             return item;
         });
 
-        labelData.forEach(newLabel => {
+        labelData.forEach((newLabel) => {
             if (!data.labels.some(label => label === newLabel)) {
                 data.labels = labelData;
                 hasChanged = true;
@@ -915,4 +806,116 @@ function resetList() {
     resetFilters();
     updateLabels();
 } //reset la page pour démarrer une nouvelle liste
+//#endregion
+
+//#region Filter
+function toggleFilter() {
+    if (!filterMenu.classList.contains('open')) {
+        filterMenu.classList.replace('closed', 'open');
+        filterButton.classList.add('is-active');
+
+        if (!filtersEventsInitiated) {
+            loadFiltersNames();
+            filtersEventsInitiated = true;
+        }        
+    }
+    else {
+        resetFilters();
+        showAllItems();
+        
+        filterMenu.classList.replace('open', 'closed');
+        filterButton.classList.remove('is-active');
+    }
+} //ouvre le menu des filtres
+
+function loadFiltersNames() {
+    const filterOptions = filterMenu.querySelectorAll('span');
+
+    filterOptions.forEach((filter, index) => {
+        const hoverHandler = () => {
+            filter.style.backgroundColor = colors[index];
+        }
+
+        const unhoverHandler = () => {
+            if (!filter.classList.contains('is-active')) {
+                filter.style.backgroundColor = 'transparent';
+            }
+        }
+
+        const clickHandler = () => {            
+            if (filter === activeFilter && filter.classList.contains('is-active')) {
+                filter.classList.remove('is-active');
+                filter.style.backgroundColor = 'transparent';
+
+                filterOptions.forEach((filter) => filter.style.opacity = 1);
+
+                activeFilter = null;
+
+                showAllItems();
+            }
+            else {
+                filterOptions.forEach((filter) => {
+                    filter.classList.remove('is-active');
+                    filter.style.backgroundColor = 'transparent';
+                    filter.style.opacity = 0.7;
+                });
+
+                filter.classList.add('is-active');
+                filter.style.backgroundColor = colors[index];
+                filter.style.opacity = 1;
+                activeFilter = filter;
+
+                activateFilter(activeFilter);
+            }
+        }
+
+        if ($.data(filter, 'events')) {
+            $(filter).off('mouseover', hoverHandler);        
+            $(filter).off('mouseout', unhoverHandler);        
+            $(filter).off('click', clickHandler);
+        } //supprimer les events
+
+        filter.addEventListener('mouseover', hoverHandler);    
+        filter.addEventListener('mouseout', unhoverHandler);    
+        filter.addEventListener('click', clickHandler);
+    });
+} //récupère les noms des labels et ajoute les listeners aux filtres 
+
+function activateFilter(activeFilter) {
+    const listItems = document.querySelectorAll('#list-container li');
+
+    listItems.forEach((item) => {
+        const labelText = item.querySelector('#label-modal p').innerText;
+        
+        if (labelText === activeFilter.innerText) {
+            item.style.display = 'flex';
+        }
+        else {
+            item.style.display = 'none';
+        }
+    })
+} //active le filtre choisi
+
+function showAllItems() {
+    const listItems = document.querySelectorAll('#list-container li');
+
+    listItems.forEach((item) => {
+        item.style.display = 'flex';
+    });
+} //montre tous les items
+
+function resetFilters() {
+    const filtersLabel = filterMenu.querySelectorAll('span');
+
+    filtersLabel.forEach((filter) => {
+        filter.classList.remove('is-active');
+        filter.style.backgroundColor = 'transparent';
+        filter.style.opacity = 1;
+    });
+
+    activeFilter = null;
+
+       
+} //reset les filtres
+
 //#endregion
